ba47c74d33511b609bef95ead178c423cbce0e7f
rickylolo <ricky_lolo29@hotmail.com>
2023-02-26 21:21:28 -0600

    JSONWebToken, Login & Register

--- controllers/user.js
+++ controllers/user.js
@@ -1,29 +1,59 @@
 const Users = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const expresJwt = require('express-jwt')
+
+const signToken = (_id) => jwt.sign({ _id }, 'hola')
 
 const User = {
+  login: async (req, res) => {
+    const user = new Users(req.body)
+    try {
+      const isUser = await Users.findOne({ email: user.email })
+      if (!isUser) {
+        res.status(403).send('Email o contraseña inválida')
+      } else {
+        const isMatch = await bcrypt.compare(user.password, isUser.password)
+        if (isMatch) {
+          const signed = signToken(isUser._id)
+          res.status(200).send(signed)
+        } else {
+          res.status(403).send('Email o contraseña inválida')
+        }
+      }
+    } catch (err) {
+      res.status(500).send(err.message)
+    }
+  },
+
   get: async (req, res) => {
     const { id } = req.params
     const user = await Users.findOne({ _id: id })
     res.status(200).send(user)
   },
+
   list: async (req, res) => {
     const users = await Users.find()
     res.status(200).send(users)
   },
-  create: async (req, res) => {
-    console.log(req.body)
-    const user = new Users(req.body)
 
-    const isUser = await Users.findOne({ email: user.email })
-    if (isUser) {
-      return res.status(403).send('Usuario ya existente')
+  create: async (req, res) => {
+    const user = new Users(req.body)
+    try {
+      const isUser = await Users.findOne({ email: user.email })
+      if (isUser) {
+        return res.status(403).send('Usuario ya existente')
+      }
+      const salt = await bcrypt.genSalt()
+      user.password = await bcrypt.hash(user.password, salt)
+      user.salt = salt
+      const signed = signToken(user.id) //Mi JSON WebToken
+
+      await user.save()
+      res.status(201).send(signed)
+    } catch (err) {
+      res.status(500).send(err.message)
     }
-    const salt = await bcrypt.genSalt()
-    user.password = await bcrypt.hash(user.password, salt)
-    user.salt = salt
-    const savedUser = await user.save()
-    res.status(201).send(savedUser.id)
   },
   update: async (req, res) => {
     const { id } = req.params
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const user = require('../controllers/user')
 
 router.get('/user', user.list)
 router.get('/user:id', user.get)
+router.get('/user/login', user.login)
 router.post('/user', user.create)
 router.put('/user:id', user.update)
 router.delete('/user:id', user.destroy)
