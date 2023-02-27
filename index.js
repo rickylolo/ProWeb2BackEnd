d85d110426b51f44a5001a7682bbde7f87367b8b
rickylolo <ricky_lolo29@hotmail.com>
2023-02-26 19:29:05 -0600

    RouteUser

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const user = require('./controllers/user')
+
+const userRoutes = require('./routes/user')
 const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use('/api', userRoutes)
 mongoose.connect(
   'mongodb+srv://user:SPt2EjQXKV3iSNxS@proweb2.djh2qpr.mongodb.net/?retryWrites=true&w=majority'
 )
-
-app.get('/', user.list)
-app.post('/', user.create)
-app.get('/:id', user.get)
-app.put('/:id', user.update)
-app.delete('/:id', user.destroy)
 
 app.get('*', (req, res) => {
   res.status(404).send('Esta página no existe')
---
+++ routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const user = require('../controllers/user')
+
+router.get('/user', user.list)
+router.get('/user:id', user.get)
+router.post('/user', user.create)
+router.put('/user:id', user.update)
+router.delete('/user:id', user.destroy)
+
+module.exports = router
