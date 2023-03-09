99b2aea4d5722f74946122a28aa7be47b8b2b415
rickylolo <ricky_lolo29@hotmail.com>
2023-03-08 23:22:46 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const userRoutes = require('./routes/user')
+const reviewRoutes = require('./routes/review')
+const productRoutes = require('./routes/product')
+const listRoutes = require('./routes/list')
+const categoryRoutes = require('./routes/category')
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use('/api', userRoutes)
+app.use('/api', reviewRoutes)
+app.use('/api', productRoutes)
+app.use('/api', listRoutes)
+app.use('/api', categoryRoutes)
+
 mongoose.connect(
   'mongodb+srv://user:SPt2EjQXKV3iSNxS@proweb2.djh2qpr.mongodb.net/?retryWrites=true&w=majority'
 )
