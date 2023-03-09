41be012b386fe8e7fb8442d76e488ffb5884d224
rickylolo <ricky_lolo29@hotmail.com>
2023-03-08 23:09:15 -0600

    product

---
+++ controllers/product.js
@@ -0,0 +1,39 @@
+const Products = require('../models/product')
+
+const product = {
+  get: async (req, res) => {
+    const { id } = req.params
+    const product = await Products.findOne({ _id: id })
+    res.status(200).send(product)
+  },
+  product: async (req, res) => {
+    const product = await Products.find()
+    res.status(200).send(product)
+  },
+  create: async (req, res) => {
+    const product = new Products(req.body)
+    try {
+      await product.save()
+      res.status(201).send('Producto Registrado correctamente')
+    } catch (err) {
+      res.status(500).send(err.message)
+    }
+  },
+  update: async (req, res) => {
+    const { id } = req.params
+    const product = await Products.findOne({ _id: id })
+    Object.assign(user, req.body)
+    await product.save()
+    res.sendStatus(204)
+  },
+  destroy: async (req, res) => {
+    const { id } = req.params
+    const product = await Products.findOne({ _id: id })
+    if (product) {
+      await user.remove()
+    }
+    res.sendStatus(204)
+  },
+}
+
+module.exports = product
---
+++ routes/product.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const product = require('../controllers/product')
+
+router.get('/product', product.product)
+router.get('/product:id', product.get)
+router.post('/product', product.create)
+router.put('/product:id', product.update)
+router.delete('/product:id', product.destroy)
+
+module.exports = router
